df9ff4a5095a046401bedaaecdbf4bc31eab160f
Chely Ho <adept_inept@yahoo.com>
2018-09-23 11:15:07 +0000

    automatically backed up by learn

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,7 +4,7 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      //solution used const cb = () => this.props.setColor(str), then onClick={cb}. any difference with this method? can't test in browser atm :(
+      //here, solution used const cb = () => this.props.setColor(str), then onClick={cb}. any difference with this method? can't test in browser atm :(
       return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={() => this.props.setColor(str)} />
     })
   )
